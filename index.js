696c9cf7e55d728cc8854edaa4e74320cf7d1a58
Rahul <rahulna1996@gmail.com>
2023-08-11 16:24:58 +0530

    Added loader

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const quoteText = document.getElementById("quote");
 const autherText = document.getElementById("auther");
 const xTwitterBtn = document.getElementById("xTwitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
+
+// Show Loading
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 // Show New Quote
 
 function newQuote() {
+  loading();
   const quote = apiQuotes.results[Math.floor(Math.random() * 150)];
   // check quote text to determine styling
   if (quote.content.length > 120) {
@@ -16,13 +32,17 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  //   Set Quote and Hide Loader
+
   quoteText.textContent = quote.content;
   autherText.textContent = quote.author;
+  complete();
 }
 
 // Get Quotes from API
 
 async function getQuotes() {
+  loading();
   const apiUrl = `https://api.quotable.io/quotes?limit=150`;
   try {
     const response = await fetch(apiUrl);
